0b8fdd686df829c4909395929f0f300492012759
Alessandro Caprarelli <alessandro.caprarelli.ext@siemens.com>
2017-11-08 18:56:25 +0100

    Added test of User model

--- web server/test/models/user.js
+++ web server/test/models/user.js
@@ -15,7 +15,9 @@ describe('User model test', function () {
     // this will run before every test to clear the database
     // TODO clear database
     before(function (done) {
-        done()
+        User.remove({}, function (err) {
+            done()
+        });
     });
 
     it('should be invalid if name is empty', function(done) {
@@ -26,4 +28,20 @@ describe('User model test', function () {
             done();
         });
     });
+
+    it('should save an user', function (done) {
+        var user = new User({
+            name: 'a',
+            surname: 'a',
+            email: 'aaa@aa.it',
+            password: 'aa',
+            role: 'participant'
+        });
+
+        user.save(function (err) {
+            expect(err).to.be.eql(null);
+            done();
+        })
+
+    })
 });
