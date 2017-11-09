bef388c6c6213c4485d504fb764902d300b7c583
Alessandro Caprarelli <alessandro.caprarelli.ext@siemens.com>
2017-11-10 00:15:47 +0100

    Fixed authentication service using properly map and catch error. Added an alerti in login form when login fails

--- web-application/src/app/_guards/authguard.guard.ts
+++ web-application/src/app/_guards/authguard.guard.ts
@@ -1,15 +1,18 @@
 import { Injectable } from '@angular/core';
-import { CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot } from '@angular/router';
-import { Observable } from 'rxjs/Observable';
-import {UserService} from "../_services/user.service";
+import { Router, CanActivate} from '@angular/router';
 
 @Injectable()
 export class AuthguardGuard implements CanActivate {
-  constructor(private user:UserService){}
+  constructor(private router: Router){}
 
-  canActivate(
-    next: ActivatedRouteSnapshot,
-    state: RouterStateSnapshot ): Observable<boolean> | Promise<boolean> | boolean {
-    return !this.user.getRole().eql('');
+  canActivate() {
+    if (localStorage.getItem('currentUser')) {
+      // logged in so return true
+      return true;
+    }
+
+    // not logged in so redirect to login page
+    this.router.navigate(['/login']);
+    return false;
   }
 }
