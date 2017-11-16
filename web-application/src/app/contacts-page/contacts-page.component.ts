e1a79114ca30aeda30496b7a56337d7e287d9a37
Ante <ante.brescic@gmail.com>
2017-11-17 00:33:28 +0100

    Added profile-page, modified contact form

--- web-application/src/app/contacts-page/contacts-page.component.ts
+++ web-application/src/app/contacts-page/contacts-page.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit, Injectable, OnChanges } from '@angular/core';
+import { FormBuilder, FormGroup, FormControl, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-contacts-page',
@@ -7,9 +8,42 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ContactsPageComponent implements OnInit {
 
-  constructor() { }
+  contactForm: FormGroup;
+
+  @Input() message = { name: '', surname: '', email: '', subject:'', message:''};
+
+  constructor(private formBuilderLogin: FormBuilder) { }
 
   ngOnInit() {
+    this.setFormContact();
+   }
+
+   /**
+   * It initializes the form with empty values.
+   */
+  setFormContact() {
+    this.contactForm = this.formBuilderLogin.group({
+      firstname: '',
+      lastname: '',
+      email: '',
+      subject: '',
+      message:''
+      });
+  }
+
+  /**
+   * It is called when the user clicks the button.
+   */
+  contact(){
+    // create an instance if user model
+    var message = {
+      name:this.contactForm.get('firstname').value,
+      surname:this.contactForm.get('lastname').value,
+      email:this.contactForm.get('email').value,
+      subject:this.contactForm.get('subject').value,
+      message:this.contactForm.get('message').value
+    }
+    console.log(message);
   }
 
 }
