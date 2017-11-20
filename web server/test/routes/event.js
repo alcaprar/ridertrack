8be7f6527ff41da165ce1ccff6f1b7dcd057c5a9
Alessandro Caprarelli <alessandro.caprarelli.ext@siemens.com>
2017-11-20 17:04:33 +0100

    Added get events by length range

--- web server/test/routes/event.js
+++ web server/test/routes/event.js
@@ -176,17 +176,34 @@ describe('Event API tests', function () {
                 "participantsList":[255],
                 "routes":["Route1"]
             });
+            var event3 = new Event({
+                "name":"TestEvent3",
+                "organizerId": mongoose.Types.ObjectId(),
+                "type":"running",
+                "description":"Blablabla",
+                "country":"MyCountry",
+                "city":"MyCity",
+                "startingTime":"2017-09-23T12:00:00.000Z",
+                "maxDuration":150,
+                "length": 60,
+                "enrollmentOpeningAt":"2017-09-10T00:00:00.000Z",
+                "enrollmentClosingAt":"2017-09-17T00:00:00.000Z",
+                "participantsList":[255],
+                "routes":["Route1"]
+            });
             event.save(function () {
                 event2.save(function () {
-                    request.get('/api/events?sort=length&length=gt:10&length=lt:40')
-                        .end(function (err1, res) {
-                            expect(res.status).to.be.eql(200);
-                            expect(res.body).to.be.an('object');
-                            expect(res.body.events).to.be.an('array');
-                            expect(res.body.events.length).to.be.eql(2);
-                            expect(res.body.events[0]._id).to.be.eql(event._id.toString());
-                            done();
-                        })
+                    event3.save(function () {
+                        request.get('/api/events?sort=length&length=gte:45&length=lt:50.5')
+                            .end(function (err1, res) {
+                                expect(res.status).to.be.eql(200);
+                                expect(res.body).to.be.an('object');
+                                expect(res.body.events).to.be.an('array');
+                                expect(res.body.events.length).to.be.eql(1);
+                                expect(res.body.events[0]._id).to.be.eql(event2._id.toString());
+                                done();
+                            })
+                    })
                 });
             });
         });
