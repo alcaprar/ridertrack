fb6754089ab49e1528ad7dfc19cbdb67891c0d00
Giulia Leonardi <giulia.leonardi@mail.polimi.it>
2017-11-23 13:21:04 +0100

    [Front-End] Footer fixed

--- web-application/src/app/shared/layout/footer/footer.component.ts
+++ web-application/src/app/shared/layout/footer/footer.component.ts
@@ -1,4 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, Input, OnInit} from '@angular/core';
+import {EventService} from "../../services/event.service";
+import {Event} from '../../models/event'
+declare var $:any;
 
 @Component({
   selector: 'app-footer',
@@ -7,7 +10,23 @@ import { Component, OnInit } from '@angular/core';
 })
 export class FooterComponent implements OnInit {
 
-  constructor() { }
+  private lastEvents: Event[];
+
+  constructor(private eventService: EventService) {
+    // retrive the last 3 events
+    this.eventService.getLastEvents(3)
+      .then(
+        (events) =>{
+          console.log('[Footer][getLastEvents][success]', events);
+          this.lastEvents = events
+        }
+      )
+      .catch(
+        (error) =>{
+          console.log('[Footer][getLastEvents][error]', error);
+        }
+      )
+  }
 
   ngOnInit() {
   }
