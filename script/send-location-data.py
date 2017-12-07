5e55864073c0f6729594ae6909d708881fb9a94e
Alessandro Caprarelli <alessandro.caprarelli.ext@siemens.com>
2017-12-08 00:56:57 +0100

    Removed comments and test function in script for sending data

--- script/send-location-data.py
+++ script/send-location-data.py
@@ -2,9 +2,6 @@
 import requests
 import json
 
-def test():
-    print "Python Working... "
-
 def create_user():
     url  = "http://localhost:4200/api/auth/register/"
     headers = {u'content-type': u'application/json'}
@@ -15,20 +12,10 @@ def create_user():
         "password": "password1234"
     }
     r = requests.post(url, json= payload, headers = headers)
-    Request.raise_for_status()
     print"POST worked!"
     print(r.content)
 
-
-def login_user():
-
-
-
-test()
-create_user()
-
-#if __name__ == "__main__":
-    #test()
-    #create_user()
+if __name__ == "__main__":
+    create_user()
 
 
