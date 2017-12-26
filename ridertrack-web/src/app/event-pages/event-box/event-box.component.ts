d4faff3973ca9d9f97bed1d312bc678106c399b7
marziadegiorgi <marziadegio@hotmail.it>
2017-12-26 11:44:32 +0100

    added the possibility to select an extra tracking device during enrollement

--- ridertrack-web/src/app/event-pages/event-box/event-box.component.ts
+++ ridertrack-web/src/app/event-pages/event-box/event-box.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit, Input } from '@angular/core';
 import {Router} from "@angular/router";
+import {DialogService} from "../../shared/dialog/dialog.service";
 
 @Component({
   selector: 'app-event-box',
@@ -14,9 +15,15 @@ export class EventBoxComponent implements OnInit {
   @Input()
   enrolled: boolean;
 
-  constructor(private router: Router) { }
+  constructor(private router: Router, private  dialogService:DialogService) { }
 
   ngOnInit() {
     console.log('[EventBox][Init]', this.event)
+  }
+
+  addDevice() {
+    this.dialogService.enrollement("Add Tracking Device", function () {
+
+    });
   }
 }
