d04f97eeba2726bd2d6d58900c645beb192bf7c5
marziadegiorgi <marziadegio@hotmail.it>
2018-01-06 19:47:47 +0100

    added timer to leaderboard

--- ridertrack-web/src/app/event-pages/event-progress/leaderboard/leaderboard.component.ts
+++ ridertrack-web/src/app/event-pages/event-progress/leaderboard/leaderboard.component.ts
@@ -16,6 +16,8 @@ export class LeaderboardComponent implements OnInit {
   private ranking: any = [];
   private started: boolean;
 
+  private refreshInterval;
+
 
   constructor(private route: ActivatedRoute, private sortService: SortService, private eventService: EventService) { }
 
@@ -25,7 +27,6 @@ export class LeaderboardComponent implements OnInit {
       this.getCurrentEvent();
       console.log('[Leaderboard][OnInit][EventId]', this.eventId);
     });
-
   }
 
   getCurrentEvent() {
@@ -34,14 +35,20 @@ export class LeaderboardComponent implements OnInit {
       if(event.status === 'ongoing'){
         this.started = true;
         console.log("[LeaderBoard][Ranking][EventStarted]", this.currentEvent.status);
-        this.eventService.getRanking(this.eventId).then((users)=> {
+        this.refreshInterval = setInterval(this.eventService.getRanking(this.eventId).then((users)=> {
           for(let i=0; i< users.length; i++){
             this.ranking.push({position: i+1, name: users[i].name, surname: users[i].surname});
           }
           console.log("[LeaderBoard][Ranking][OnInit]", this.ranking);
           this.sortService.sortTable({sortColumn: 'position', sortDirection:'asc'}, this.ranking);
-        })
-      }else {
+        }),5 * 1000 );
+      }
+      if(event.status === 'passed') {
+        this.started = false;
+        console.log("[LeaderBoard][Ranking][EventPassed]");
+        //TODO: GET FINAL RANKING
+      }
+      if(event.status === 'planned') {
         this.started = false;
         console.log("[LeaderBoard][Ranking][EventNotStarted]");
       }
